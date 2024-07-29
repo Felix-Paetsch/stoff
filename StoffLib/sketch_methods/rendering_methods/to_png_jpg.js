3f902e4d3cf929e31d056007dc67fcb86134fb74
Ellyly <felixpaetsch@gmail.com>
2024-07-29 15:42:30 +0200

    initial video

--- StoffLib/sketch_methods/rendering_methods/to_png_jpg.js
+++ StoffLib/sketch_methods/rendering_methods/to_png_jpg.js
@@ -3,7 +3,7 @@ import { writeFileSync } from 'fs';
 import { sketch_to_renderable, calculate_correct_width_height } from './sketch_to_renderable.js';
 import { interpolate_colors } from '../../colors.js';
 
-function create_png_from_sketch(s , width, height){
+function create_canvas_from_sketch(s , width, height){
     const correct_dimensions = calculate_correct_width_height(s, width, height);  
     const {
         bb,
@@ -58,14 +58,31 @@ function create_png_from_sketch(s , width, height){
     lines.forEach(drawPolyline);
     points.forEach(drawCircle);
 
-    return canvas.toBuffer();
+    return canvas;
 }
 
-export { create_png_from_sketch, save_as_png };
-function save_as_png(sketch, save_to, width, height, to_lifesize = false) {
-    const pngBuffer = create_png_from_sketch(sketch, width, height, to_lifesize);
+function create_png_from_sketch(s , width, height){
+    return create_canvas_from_sketch(s, width, height).toBuffer();
+}
+
+function create_jpg_from_sketch(s , width, height){
+    return create_canvas_from_sketch(s, width, height).toBuffer('image/jpeg');
+}
+
+export { create_png_from_sketch, save_as_png, create_jpg_from_sketch, save_as_jpg  };
+
+function save_as_png(sketch, save_to, width, height) {
+    const pngBuffer = create_png_from_sketch(sketch, width, height);
     writeFileSync(save_to, pngBuffer, (err) => {
         if (err) throw err;
         console.log('PNG file saved!');
+    });
+}
+
+function save_as_jpg(sketch, save_to, width, height) {
+    const jpgBuffer = create_jpg_from_sketch(sketch, width, height);
+    writeFileSync(save_to, jpgBuffer, (err) => {
+        if (err) throw err;
+        console.log('JPG file saved!');
     });
 }
---
+++ Video/generate_setting.js
@@ -0,0 +1,16 @@
+import CONF from './config.json' assert { type: 'json' };
+
+function* generate_setting(design_config){
+    let frame = 0;
+    while (true){
+        const res = design_config.to_obj();
+        res.frame = frame;
+        yield res;
+        frame++;
+    }
+}
+
+export default function _generate_setting(design_config){
+    const gen = generate_setting(design_config);
+    return () => gen.next().value;
+}
