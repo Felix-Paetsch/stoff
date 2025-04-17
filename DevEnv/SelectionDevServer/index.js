c8d1cec8f3de2a2abe29719b57709dd903410a13
Ellyly <felixpaetsch@gmail.com>
2025-04-17 11:45:51 +0200

    Setup devSel

---
+++ DevEnv/SelectionDevServer/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3008, () => {
+  console.log('Server running on http://localhost:3008');
+});
