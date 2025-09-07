118e63c16d2c5b50926adf08111640bb0eba0030
Saitha <ulbriali@b-tu.de>
2025-09-07 15:38:33 +0200

    some functions for manual construction

--- Patterns/manual_construction.js
+++ Patterns/manual_construction.js
@@ -17,15 +17,20 @@ export function construct_maual(sketches) {
     const data = {
       side: "overlock",
       shoulder: "knappnaht",
-      neckline: "",
+      neckline: "schrägband",
       hem: "",
       back: "",
       darts: "",
+      sleeve: {
+        no_sleeve: false,
+        hem: "",
+        seam: "eingereiht"
+      }
 
     }
 
-    close_darts(sketches, s);
-    close_waistline_darts(sketches, s);
+    close_darts(sketches, s, data.darts);
+    close_waistline_darts(sketches, s, data.darts);
 
 // noch keine Raglan aermel, sonst erst seite, dann ärmel, dann halsausschnitt
 // wenn fancy knopfleiste o.ä. dann ggf. vor schulter
@@ -33,19 +38,23 @@ export function construct_maual(sketches) {
 
     // schulter, halsausschnitt, hinten, seite, armausschnitt, saum
 
-    close_shoulder(sketches, s);
+    close_shoulder(sketches, s, data.shoulder);
 
-    sew_neckline(sketches, s);
+    sew_neckline(sketches, s, data.neckline);
 
-    close_back(sketches, s);
+    close_back(sketches, s, data.back);
 
-    close_side(sketches, s);
+    close_side(sketches, s, data.side);
+
+    sew_sleeve(sketches, s, data.sleeve);
+
+    sew_hem(sketches, s, data.hem);
 
 
     return s;
 }
 
-function close_darts(sketches, s) {
+function close_darts(sketches, s, sew_type) {
     hot_at_url(s, "/wha");
 
     sketches.forEach((sketch) => {
@@ -59,15 +68,15 @@ function close_darts(sketches, s) {
             const darts = l_left.length / 3;
             for (let i = 1; i <= darts; i++) {
                 let lns = l_left.filter((ln) => ln.data.dart_number == i);
-                close_dart(s, lns);
+                close_dart(s, lns, sew_type);
                 lns = l_right.filter((ln) => ln.data.dart_number == i);
-                close_dart(s, lns);
+                close_dart(s, lns, sew_type);
             }
         }
     });
 }
 
-function close_dart(s, lines) {
+function close_dart(s, lines, sew_type) {
     if (lines.length > 0) {
         let ln = lines.filter((ln) => ln.data.type == "annotation");
         let lns = lines.filter((ln) => ln.data.type == "dart");
@@ -82,13 +91,13 @@ function close_dart(s, lines) {
                 same_orientation: true,
                 same_handedness: false,
             },
-        ]);
+        ], sew_type);
 
         s.hightlight(...s.sewing_points)
     }
 }
 
-function close_waistline_darts(sketches, s) {
+function close_waistline_darts(sketches, s, sew_type) {
     sketches.forEach((sketch) => {
         const lines_map = sketch.lines_by_key("sewing");
         if (Object.keys(lines_map).includes("close_waistline_dart")) {
@@ -98,19 +107,19 @@ function close_waistline_darts(sketches, s) {
             const l_right = lines.filter((ln) => !ln.right_handed);
 
             let lns = l_left.filter((ln) => ln.data.position == "inner");
-            close_dart(s, lns);
+            close_dart(s, lns, sew_type);
             lns = l_right.filter((ln) => ln.data.position == "inner");
-            close_dart(s, lns);
+            close_dart(s, lns, sew_type);
 
             lns = l_left.filter((ln) => ln.data.position == "outer");
-            close_dart(s, lns);
+            close_dart(s, lns, sew_type);
             lns = l_right.filter((ln) => ln.data.position == "outer");
-            close_dart(s, lns);
+            close_dart(s, lns, sew_type);
         }
     });
 }
 
-function close_shoulder(sketches, s){
+function close_shoulder(sketches, s, sew_type){
   let lns1 = [];
   let lns2 = [];
   sketches.forEach((sketch, i) => {
@@ -129,11 +138,11 @@ function close_shoulder(sketches, s){
         }
       }
   });
-  //s.sew(lns1[0], lns1[1]);
-  //s.sew(lns2[0], lns2[1]);
+//  s.sew(lns1[0], lns1[1], sew_type);
+//  s.sew(lns2[0], lns2[1], sew_type);
 }
 
-function close_side(sketches, s){
+function close_side(sketches, s, sew_type){
   let lns1 = [];
   let lns2 = [];
   sketches.forEach((sketch, i) => {
@@ -150,16 +159,51 @@ function close_side(sketches, s){
         }
       }
   });
-  //s.sew(lns1[0], lns1[1]);
-  //s.sew(lns2[0], lns2[1]);
+  //s.sew(lns1[0], lns1[1], sew_type);
+  //s.sew(lns2[0], lns2[1], sew_type);
 }
 
 // Wird aktuell nicht verwendet
-function close_back(sketches, s){
+function close_back(sketches, s, sew_type){
   let lns1 = [];
   let lns2 = [];
   sketches.forEach((sketch, i) => {
       const lines = sketch.get_typed_lines("back_cut");
+      if (lines) {
+        s.sew(s.sewing_line(lines[0]), s.sewing_line(lines[1]), sew_type);
+      }
+  });
+}
+
+function sew_neckline(sketches, s, sew_type){
+  let lns = [];
+  sketches.forEach((sketch, i) => {
+      const lines = sketch.get_typed_lines("neckline");
+      if (lines) {
+        lns.push(s.merge_lines(lines));
+      }
+  });
+  let line = s.merge_lines(lns);
+  s.todo(line, sew_type);
+}
+
+function sew_hem(sketches, s, sew_type){
+  let lns = [];
+  sketches.forEach((sketch, i) => {
+      const lines = sketch.get_typed_lines("bottom");
+      if (lines) {
+        lns.push(s.merge_lines(lines));
+      }
+  });
+  let line = s.merge_lines(lns);
+  s.todo(line, sew_type);
+}
+
+function sew_sleeve(sketches, s, sew_type){
+  let lns1 = [];
+  let lns2 = [];
+  sketches.forEach((sketch, i) => {
+      const lines = sketch.get_typed_lines("armpit");
       if (lines) {
         const l_left = lines.filter((ln) => ln.right_handed);
         const l_right = lines.filter((ln) => !ln.right_handed);
@@ -172,18 +216,14 @@ function close_back(sketches, s){
         }
       }
   });
-  //s.sew(lns1[0], lns1[1]);
-  //s.sew(lns2[0], lns2[1]);
-}
+  let line1 = s.merge_lines(lns1);
+  let line2 = s.merge_lines(lns2);
 
-function sew_neckline(sketches, s){
-  let lns1 = [];
-  let lns2 = [];
-  sketches.forEach((sketch, i) => {
-      const lines = sketch.get_typed_lines("back_cut");
-      if (lines) {
-      }
-
-  });
-
+  if (sew_type.no_sleeve){
+    s.todo(line, sew_type.hem);
+    s.todo(line2, sew_type.hem);
+  } else {
+    // ToDo Ärmel dublizieren und hier Funktionen für die Sketches machen
+    // Danach die Linien von da mit dem Armloch vernähen
+  }
 }
