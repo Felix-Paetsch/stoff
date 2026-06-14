230ccac6dfa32eba10c2252bf800cc34bebdb631
Felix <felixpaetsch@gmail.com>
2026-06-14 18:01:16 +0200

    Faster Merge Polylines

--- Rust/src/geometry/algorithms/merge_shapes/merge_polylines.rs
+++ Rust/src/geometry/algorithms/merge_shapes/merge_polylines.rs
@@ -1,19 +1,7 @@
-use crate::debug::*;
-
 use crate::geometry::utils::distance_graph::distance_graph;
 use crate::geometry::{Polygon, Polyline, ShapeT, Vector};
 use crate::graph::algorithms::minimum_weight_perfect_matching::min_weight_matching_f64;
-use petgraph::unionfind::UnionFind;
 
-struct MergePair {
-    a_poly: usize,
-    a_end: usize,
-    b_poly: usize,
-    b_end: usize,
-}
-
-// Should be good for maybe up to 10_000?
-// Could be improved with grid based lookups?
 pub fn merge_polylines(lns: Vec<Polyline>) -> (Polyline, Vec<Polygon>) {
     debug_assert_ne!(lns.len(), 0);
     debug_assert!(lns.iter().all(|l| !l.is_empty()));
@@ -26,102 +14,107 @@ pub fn merge_polylines(lns: Vec<Polyline>) -> (Polyline, Vec<Polygon>) {
         points.push(l.vertex_at(l.vertex_count() - 1));
     }
 
-    let (raw_pairs, _) = pair_all_but_two(&points);
+    let (raw_pairs, left_over) = pair_all_but_two(&points);
 
-    let mut shapes: Vec<Option<Vec<Vector>>> =
-        lns.into_iter().map(|pl| Some(pl.into_vertices())).collect();
+    let shapes: Vec<Vec<Vector>> = lns.into_iter().map(|pl| pl.into_vertices()).collect();
+    let mut shape_connection_graph: Vec<Option<usize>> = vec![None; 2 * poly_count];
 
-    let mut uf = UnionFind::<usize>::new(poly_count);
-    let mut component_is_polygon = vec![false; poly_count];
+    raw_pairs.into_iter().for_each(|(a, b)| {
+        shape_connection_graph[a] = Some(b);
+        shape_connection_graph[b] = Some(a);
+    });
 
-    for (a, b) in raw_pairs {
-        let pair = MergePair {
-            a_poly: a / 2,
-            a_end: a % 2,
-            b_poly: b / 2,
-            b_end: b % 2,
-        };
+    let mut shape_visitation_tracker: Vec<bool> = vec![false; poly_count];
 
-        let ra = uf.find(pair.a_poly);
-        let rb = uf.find(pair.b_poly);
+    // First do the out polyline so we know the rest are cycles
+    let out_polyline: Polyline = {
+        Polyline::new(merge_vertices(
+            left_over.0,
+            &shapes,
+            &shape_connection_graph,
+            &mut shape_visitation_tracker,
+        ))
+    };
 
-        if ra == rb {
-            component_is_polygon[ra] = true;
+    let mut out_polygons: Vec<Polygon> = Vec::new();
+    for shape_index in 0..poly_count {
+        if shape_visitation_tracker[shape_index] {
             continue;
         }
 
-        let va = shapes[ra].take().unwrap();
-        let vb = shapes[rb].take().unwrap();
-
-        let merged = merge_shape_vectors(va, pair.a_end, vb, pair.b_end);
-        uf.union(ra, rb);
-        let r = uf.find(ra);
-
-        shapes[r] = Some(merged);
-        debug_assert!(!component_is_polygon[r]);
+        out_polygons.push(Polygon::new(merge_vertices(
+            2 * shape_index,
+            &shapes,
+            &shape_connection_graph,
+            &mut shape_visitation_tracker,
+        )))
     }
 
-    let mut merged_polyline = None;
-    let mut polygons = Vec::new();
-
-    for i in 0..poly_count {
-        let r = uf.find(i);
-        if r != i {
-            continue;
-        }
-
-        let vertices = shapes[i]
-            .take()
-            .expect("root should always have shape vertices");
-
-        if component_is_polygon[i] {
-            polygons.push(Polygon::new(vertices));
-        } else {
-            debug_assert!(merged_polyline.is_none());
-            merged_polyline = Some(Polyline::new(vertices));
-        }
-    }
-
-    (
-        merged_polyline.expect("expected exactly one non-polygon component"),
-        polygons,
-    )
+    (out_polyline, out_polygons)
 }
 
-fn merge_shape_vectors(
-    mut a: Vec<Vector>,
-    a_end: usize,
-    mut b: Vec<Vector>,
-    b_end: usize,
+fn merge_vertices(
+    initial_startpoint_index: usize,
+    shapes: &[Vec<Vector>],
+    shape_connection_graph: &[Option<usize>],
+    shape_visitation_tracker: &mut [bool],
 ) -> Vec<Vector> {
-    match (a_end, b_end) {
-        (1, 0) => {
-            a.extend(b);
-            a
+    let mut total_len: usize = 0;
+    let mut current_line_startpoint_index = initial_startpoint_index;
+
+    loop {
+        total_len += shapes[current_line_startpoint_index / 2].len();
+        let current_line_endpoint_index = current_line_startpoint_index ^ 1;
+
+        if let Some(next_line_startpoint_index) =
+            shape_connection_graph[current_line_endpoint_index]
+        {
+            if next_line_startpoint_index == initial_startpoint_index {
+                break;
+            }
+
+            current_line_startpoint_index = next_line_startpoint_index
+        } else {
+            break;
         }
-        (1, 1) => {
-            b.reverse();
-            a.extend(b);
-            a
-        }
-        (0, 0) => {
-            a.reverse();
-            a.extend(b);
-            a
-        }
-        (0, 1) => {
-            b.extend(a);
-            b
-        }
-        _ => unreachable!(),
     }
+
+    let mut res: Vec<Vector> = Vec::with_capacity(total_len);
+
+    let mut current_line_startpoint_index = initial_startpoint_index;
+
+    loop {
+        shape_visitation_tracker[current_line_startpoint_index / 2] = true;
+
+        if current_line_startpoint_index.is_multiple_of(2) {
+            res.extend_from_slice(&shapes[current_line_startpoint_index / 2]);
+        } else {
+            res.extend(shapes[current_line_startpoint_index / 2].iter().rev());
+        }
+
+        let current_line_endpoint_index = current_line_startpoint_index ^ 1;
+
+        if let Some(next_line_startpoint_index) =
+            shape_connection_graph[current_line_endpoint_index]
+        {
+            if next_line_startpoint_index == initial_startpoint_index {
+                break;
+            }
+
+            current_line_startpoint_index = next_line_startpoint_index
+        } else {
+            break;
+        }
+    }
+
+    debug_assert_eq!(res.len(), total_len);
+    res
 }
 
-pub fn pair_all_but_two(points: &[Vector]) -> (Vec<(usize, usize)>, (usize, usize)) {
+fn pair_all_but_two(points: &[Vector]) -> (Vec<(usize, usize)>, (usize, usize)) {
     let distance_graph = distance_graph(points);
 
     let mut matching = min_weight_matching_f64(&distance_graph);
-    debug_log_1(&matching);
 
     let mut curr_max_distance = points[matching[0].0].distance(points[matching[0].1]);
     let mut curr_max_index = 0;
